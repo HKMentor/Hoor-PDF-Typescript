24e9e67ed0ad3d68406894fdb184537b9ccaebdd
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-06-26 16:33:23 +0500

    hello world

---
+++ step01/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// console.log("Hello World");
+// // core types
+// 1: string ="name";
+// 2: number = 342;
+// 3: boolean = true / false
+//........Variable............//
+let name1 = "hooria";
+console.log(name1);
+let num = 123;
+console.log(num);
+let isMarried = true;
+console.log(isMarried);
---
+++ step01/main.ts
@@ -0,0 +1,23 @@
+// console.log("Hello World");
+
+
+// // core types
+// 1: string ="name";
+// 2: number = 342;
+// 3: boolean = true / false
+
+//........Variable............//
+let name1:string="hooria";
+
+console.log(name1);
+
+let num:number= 123;
+
+console.log(num);
+
+let isMarried:boolean=true;
+
+console.log(isMarried);
+
+let 
+
