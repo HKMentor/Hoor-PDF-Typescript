cddf872e3e06965549dad05588d6eae07a88f0ba
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-06-26 17:00:29 +0500

    union type

---
+++ step09/main.js
@@ -0,0 +1,13 @@
+//union type
+// type example 1......
+var mobile = "Iphone Pro max";
+mobile = "Max pro 14";
+mobile = 2022;
+mobile = undefined;
+mobile = true;
+// type example 2.....
+var value;
+value = "Hello Hoor"; //valid
+console.log("String:".concat(value));
+value = 56; //valid
+console.log("Number:".concat(value));
---
+++ step09/main.ts
@@ -0,0 +1,24 @@
+
+//union type
+ 
+// type example 1......
+let mobile:string | number | undefined   = "Iphone Pro max";
+
+mobile = "Max pro 14";
+
+mobile = 2022;
+
+mobile = undefined;
+
+mobile = true;
+
+
+// type example 2.....
+let value:string | number ;
+
+value = "Hello Hoor"; //valid
+console.log(`String:${value}`);
+
+value =  56;     //valid
+console.log(`Number:${value}`);
+
