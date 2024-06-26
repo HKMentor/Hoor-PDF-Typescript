4968e2690160c518661475887d50319f0299c87e
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-06-26 16:57:59 +0500

    if & else

---
+++ step07/main.js
@@ -0,0 +1,43 @@
+//<<<<<<<<<<<If And If-else Statement>>>>>>>>>>
+// If Statement Example 
+// Senario : checking if its a weekend?
+// //example 1.
+// let day:string = "Saturday";
+// if (day === 'Saturday'){
+//     console.log("It's a weekend.")
+// };
+// //example 2.
+// let name1:string = "Hoor";
+// let name2:string = "khan";
+// if(name1 == 'hoor' || name2 == 'khan'){
+//     console.log("Hello! hoor khan.")
+// };
+// //If-else Statement....
+// // Scenario: Checking if someone is old enough to vote.
+// //example 1.
+// let age:number = 17;
+// if(age >= 18){
+//     console.log("You can vote.");
+// }else{
+//     console.log("You cannot vote!")
+// };
+// // example 2.
+// let gate:string = "Mk";
+// if(gate === 'MK'){
+//     console.log("Plz! come here MK.");
+// }else{
+//     console.log("No, You don't come!")
+// };
+// if-else & else-if statemennt
+// example 1
+var hour = 14;
+if (hour < 12) {
+    console.log('Good Morning!');
+}
+else if (hour < 18) {
+    console.log('Good Afternoon!');
+}
+else {
+    console.log('Good Evening!');
+}
+;
