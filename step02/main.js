7eb48ef847b4de3026e9f91b89a0b2c8129b20d6
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-06-26 16:46:31 +0500

    variables

---
+++ step02/main.js
@@ -0,0 +1,15 @@
+// // core types
+// 1: string ="name";
+// 2: number = 342;
+// 3: boolean = true / false
+//........Variable............//
+var name1 = "hooria";
+console.log(name1);
+var num = 123;
+console.log(num);
+var isMarried = true;
+console.log(isMarried);
+var y;
+console.log(y);
+var z = null;
+console.log(z);
---
+++ step02/main.ts
@@ -0,0 +1,32 @@
+// // core types
+// 1: string ="name";
+// 2: number = 342;
+// 3: boolean = true / false
+
+//........Variable............//
+let name1:string="hooria";
+
+console.log(name1);
+
+let num:number= 123;
+
+console.log(num);
+
+let isMarried:boolean=true;
+
+console.log(isMarried);
+
+let y;
+
+console.log(y); //undefine
+
+
+let z = null;
+
+console.log(z);  //null
+
+
+
+
+
+
