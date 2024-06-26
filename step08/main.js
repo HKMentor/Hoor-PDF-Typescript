9b94474d740e4e05c572ea7caa8adbe85bfed098
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-06-26 16:59:13 +0500

    loop

---
+++ step08/main.js
@@ -0,0 +1,16 @@
+// //<<<<<<<<<< Loop >>>>>>>>>>
+// //  example 1............
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);           //Output: 4 times print counting 1 to 4 
+// }
+// // example 2...............
+// for (let i = 1; i <= 5; i++) {
+//     console.log("Hello loop!");    //Output: 5 times print Hello loop!
+// }
+//<<<<<<<<< Nested Loop >>>>>>
+for (var i = 0; i < 2; i++) {
+    console.log('Hello Nested loop!');
+    for (var j = 0; j < 3; j++) {
+        console.log("My name is Hooria Khan");
+    }
+}
