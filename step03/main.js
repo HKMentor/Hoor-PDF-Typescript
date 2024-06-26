d8709260040316632647c2cc057ee3059392db91
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-06-26 16:48:27 +0500

    array

---
+++ step03/main.js
@@ -0,0 +1,41 @@
+//<<<<< Array >>>>>>>>>>>>>>
+// let Fruits:string[] = ['mango','banana','apple','orange','kiwi'];
+// console.log(Fruits);
+// console.log(Fruits[0]);
+// console.log(Fruits[1]);
+// console.log(Fruits[2]);
+// console.log(Fruits[3]);
+// console.log(Fruits[4]);
+//<<<< nested array >>>>
+// let arr = ['khadim','huzair', ['Mujtuba','Huzaifa']];
+// console.log('Array:',arr);
+// console.log('Index 0:',arr[0]);
+// console.log('Index 1:',arr[1]);
+// console.log('Index 2:',arr[2]);
+// console.log('nested arr index 0:',arr[2][0]);
+// console.log('nested arr index 1:',arr[2][1]);
+//<< Pop Method >>
+// let arr:number[] = [1,2,3];
+// arr.pop();
+// console.log(arr);  // Output: [1 ,2]
+//<< Push Method >>
+// let arr:number[] = [1,2,3];
+// arr.push(4);
+// console.log(arr);  //Output: [1,2,3,4]
+// //<< Shift Method >>
+// let arr:number[] = [1,2,3];
+// arr.shift();
+// console.log(arr);   //Output: [2,3]
+// //<< Unshift >>
+// let arr:number[] = [2,3];
+// arr.unshift(1);
+// console.log(arr);   //Output: [1,2,3]
+//<< Slice >>
+var arr = [1, 2, 3, 4,];
+// let slicedArray:number[] = arr.slice(1,3);
+arr.slice(1);
+console.log(arr); //Output:[2,3]
+// //<< Splice >>
+// let arr:number[] = [1,2,3,4,5];
+// arr.splice(1,2);
+// console.log(arr);   //Output: [1,4,5]
