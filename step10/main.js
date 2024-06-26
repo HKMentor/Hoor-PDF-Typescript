e7399f6bd10cf849c3ae848173e898cc07cb8ddb
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-06-26 17:01:39 +0500

    type allias

---
+++ step10/main.js
@@ -0,0 +1,15 @@
+// enum Months {
+//     Jan,
+//     Feb,
+//     Mar,
+//     Apr,
+//     May,
+//     Jun,
+//     July,
+//     Aug,
+//     Sep,
+//     Oct,
+//     Nov,
+//     Dec
+var favoriteColor = 1 /* Color.Green */;
+console.log(favoriteColor); // Outputs: 1
---
+++ step10/main.ts
@@ -0,0 +1,49 @@
+// enum Months {
+//     Jan,
+//     Feb,
+//     Mar,
+//     Apr,
+//     May,
+//     Jun,
+//     July,
+//     Aug,
+//     Sep,
+//     Oct,
+//     Nov,
+//     Dec
+
+// };
+// console.log(Months);
+
+// enum Months {
+//     Jan = 2,
+//     Feb ,
+//     Mar ,
+//     Apr ,
+//     May ,
+//     Jun ,
+// };
+// console.log(Months);
+
+
+///Example -1 Traffic light
+const enum Color {    //index num 
+    Red,                //0
+    Green,              //1
+    Orange              //2
+  }
+ 
+  console.log(Color); // Error
+
+// ///Example -1 Traffic light
+// const enum Color {    //index num 
+//     Red,                //0
+//     Green,              //1
+//     Orange              //2
+//   }
+  
+//   let favoriteColor: Color = Color.Green;
+  
+//   console.log(favoriteColor); // Outputs: 1
+
+
